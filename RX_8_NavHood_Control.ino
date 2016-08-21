dasonwong/rx8navhood

commit 5db611c418399a0d7aa266cddea1da6b77bcdcc6

Fixed a bug where the checkOff() function would keep running when the car was already off
checkOff() will now only run if the ACCPIN is LOW and the carOff is false.

RX_8_NavHood_Control.ino
@@ -68,13 +68,13 @@ void loop() {
   /*
    * The main function, runs continuously
    */
-  if (digitalRead(ACCPIN) == HIGH) {    // Accessories are on (car on)
-    checkResume();                      // Check if we the car was previously off
-    checkOpenButton();                  // Check if the Open button has been pressed
-    checkTiltButton();                  // Check if the Tilt button has been pressed
+  if (digitalRead(ACCPIN) == HIGH) {                    // Accessories are on (car on)
+    checkResume();                                      // Check if we the car was previously off
+    checkOpenButton();                                  // Check if the Open button has been pressed
+    checkTiltButton();                                  // Check if the Tilt button has been pressed
   }
 
-  if (digitalRead(ACCPIN) == LOW) {     // Accessories are off (car off)
+  if (digitalRead(ACCPIN) == LOW && carOff == false) {  // Accessories are off (car off)
     checkOff();
   }
 }

commit 1046565d141d0041bdbe78e7b258f475ca8fb40a

Fixed a bug preventing the Arduino from sleeping
The no sleep bug was introduced in commit 5db611c when I stopped checkOff from running when the ACC was off. This also stopped the car off timer and sleep functions from running. I've moved the car off timer into its own function.

RX_8_NavHood_Control.ino
@@ -75,7 +75,11 @@ void loop() {
   }
 
   if (digitalRead(ACCPIN) == LOW && carOff == false) {  // Accessories are off (car off)
-    checkOff();
+    if (carOff == false) {
+      checkOff();
+    }else{
+      checkCarOffTime();
+    }
   }
 }
 
@@ -136,7 +140,7 @@ void checkTiltButton() {
 
 void checkOff() {
   /*
-   * Check if the car is off, close the hood and set a limit on how long to charge the tablet after the car is off
+   * Check if the car is off and closes the hood
    */
   delay(ACCDETECTDELAY);                                          // Wait a ACCDETECTDELAY time
   if (digitalRead(ACCPIN) == LOW) {                               // Check if Accessories is still off
@@ -152,6 +156,20 @@ void checkOff() {
       sleepNow();                                                 // Make Arduino go into sleep mode
     }
   }
+}
+
+void checkCarOffTime() {
+  /*
+   * Check how long the car has been off and puts the Arduino to sleep
+   */
+  carOffTime += 2;                                                // Increment the car off timer (s)
+  if (carOffTime > CAROFFCHARGETIME) {                            // Car has been off for > CAROFFCHARGETIME
+    Serial.println("Entering sleep mode");
+    digitalWrite(CHARGEENABLE, LOW);                              // Stop charging the tablet
+    delay(100);
+    sleepNow();                                                   // Make Arduino go into sleep mode
+  }
+  delay(2000);
 }
 
 void restorePosition() {
@@ -220,4 +238,3 @@ void wakeUp() {
    */
   Serial.println("Resuming from sleep");
 }
-